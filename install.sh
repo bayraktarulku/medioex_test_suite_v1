08d172e9245a9ca474d2a1a462ac53f6396d7b66
Kenobi <nejdetyucesoy@gmail.com>
2017-03-23 14:26:11 +0300

    fix attempt #1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # bcm2835 install
 wget https://github.com/pe2a/MedIOEx/blob/master/bcm2835-1.50.tar.gz
-tar zxvf bcm2835-1.50.tar.gz
+tar xvf bcm2835-1.50.tar.gz
 cd bcm2835-1.50
 ./configure
 make
@@ -13,13 +13,11 @@ cd ..
 # Download a release for medioex
 wget https://github.com/nejdetckenobi/pymedioex/archive/py34.tar.gz
 tar zxvf py34.tar.gz
-cd pymedioex-py34
-python3 setup.py install
+(cd pymedioex-py34 & python3 setup.py install)
 
 # Clone test suite
 git clone https://github.com/nejdetckenobi/medioex_test_suite
-cd medioex_test_suite
-pip3 install -r requirements.txt
+(cd medioex_test_suite & pip3 install -r requirements.txt)
 
 # Cleaning up
 rm -rf bcm2835-1.50*
